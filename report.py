70f6f337ab4547f2836b4aa4ec853d2ff654a516
calzettidaniele <calzettidaniele@gmail.com>
2021-12-11 11:51:41 +0100

    Fixed json_flag argument

--- report.py
+++ report.py
@@ -24,6 +24,7 @@ def report_generator(search_results, json_flag):
                 descriptor_list.append(fields["title"])
                 descriptor_list.append(fields["snippet"])
                 writer.writerow(descriptor_list)
-    
-    with open("".join([search_engine_json["search_parameters"]["engine"], ".json"]), "w", encoding="utf-8") as file:
-        json.dump(search_engine_json, file)
+
+    if json_flag == True:
+        with open("".join([search_engine_json["search_parameters"]["engine"], ".json"]), "w", encoding="utf-8") as file:
+            json.dump(search_engine_json, file)
